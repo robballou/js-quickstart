8f2afca730a5bc2f7486fa77d60b6823ad9fa217
Rob Ballou <rob.ballou@bombbomb.com>
2019-03-29 14:12:52 -0600

    Added scss support

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import './App.scss';
+
 export class App extends React.Component {
-  render() {
-    return (<div>Hello, World</div>);
-  }
+    render() {
+        return (<div className='app'>
+            <h1>JS Quickstart</h1>
+            Hello, World
+        </div>);
+    }
 }
